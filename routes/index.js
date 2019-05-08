f38f168355a141441ac878d460d77af7feef7e48
Stephen Eyerly <seyerly@seas.upenn.edu>
2019-05-08 14:37:05 -0400

    added in framework code. It is working so far. Trying to get images to work

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+path = require('path')
+
+var path = path.join(__dirname, '../views/');
+
+router.get('/', (req, res) => {
+    res.sendFile(path + "index.html");
+});
+
+module.exports = router;
