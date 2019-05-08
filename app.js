e2e18e4caa752bf13e872d097b7f4953f53ba66c
Stephen Eyerly <seyerly@seas.upenn.edu>
2019-05-08 15:30:02 -0400

    got issue with express not serving static images resolved

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const routes = require('./routes/index');
 
 const app = express();
 app.use('/', routes);
+app.use(express.static('images'))
+app.use(express.static('views'))
 
 module.exports = app;
 
